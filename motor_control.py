3cc20e0873b7d8f56049af1f423f77bd1849797a
nadavk <kahanobeats@gmail.com>
2022-09-29 15:01:59 +0300

    step counter configured

--- motor_control.py
+++ motor_control.py
@@ -1,28 +1,30 @@
 import RPi.GPIO as GPIO
 import time
 import multiprocessing
+from multiprocessing.managers import BaseManager
 
 
 class Motor:
-    def __init__(self, motor_name, direction_pin, step_pin, motor_off_switch_pin, direction):
+    def __init__(self, motor_name, direction_pin, step_pin, off_switch_pin, direction, step_amount):
         self.motor_name = motor_name
         self.direction_pin = direction_pin
         self.step_pin = step_pin
-        self.motor_off_switch_pin = motor_off_switch_pin
+        self.off_switch_pin = off_switch_pin
         self.step_counter = 0
         self.direction = direction
         self.velocity = 0
         self.current_position = 0
         self.motor_setup()
-        self.motor_switch_off = GPIO.input(self.motor_off_switch_pin)
+        self.off_switch = GPIO.input(self.off_switch_pin)
+        self.step_amount = step_amount
 
     def motor_setup(self):
         GPIO.setup(self.direction_pin, GPIO.OUT)
         GPIO.setup(self.step_pin, GPIO.OUT)
-        GPIO.setup(self.motor_off_switch_pin, GPIO.IN)
+        GPIO.setup(self.off_switch_pin, GPIO.IN)
 
     def check_motor_switch(self):
-        self.motor_switch_off = GPIO.input(self.motor_off_switch_pin)
+        self.off_switch = GPIO.input(self.off_switch_pin)
 
     def motor_single_step(self, velocity):
         GPIO.output(self.step_pin, GPIO.HIGH)
@@ -30,20 +32,49 @@ class Motor:
         GPIO.output(self.step_pin, GPIO.LOW)
         time.sleep(0.5 * velocity)
 
-    def update_motor_status(self):
+    def update_motor_status(self, velocity, direction):
         self.step_counter += 1
+        self.velocity = velocity
+        self.direction = direction
 
         if self.direction == 1:
             self.current_position += 1
         else:
             self.current_position -= 1
+        self.monitor_current_position()
+
+    def monitor_current_position(self):
+        if self.current_position == self.step_amount:
+            self.current_position = 0
+
+        if self.current_position < 0:
+            self.current_position = (self.step_amount - 1) + self.current_position
 
     def motor_loop(self, velocity, direction):
+
         GPIO.output(self.direction_pin, direction)
-        while self.motor_switch_off and velocity != 0:  # and self.step_counter <= 200:
+        while self.off_switch and velocity != 0:  # and self.step_counter < 200:
             self.motor_single_step(velocity)
-            self.update_motor_status()
+            self.update_motor_status(velocity, direction)
             self.check_motor_switch()
+
+    def get_values(self):
+        return {'motor_name': self.motor_name,
+                'direction_pin': self.direction_pin,
+                'step_pin': self.step_pin,
+                'off_switch_pin': self.off_switch_pin,
+                'step_counter': self.step_counter,
+                'direction': self.direction,
+                'velocity': self.velocity,
+                'current_position': self.current_position,
+                'off_switch': self.off_switch}
+
+    def get_dynamic_values(self):
+        return {'step_counter': self.step_counter,
+                'direction': self.direction,
+                'velocity': self.velocity,
+                'current_position': self.current_position,
+                'off_switch': self.off_switch}
 
 
 if __name__ == "__main__":
@@ -54,26 +85,27 @@ if __name__ == "__main__":
     # Set board's GPIO pins
     GPIO.setmode(GPIO.BOARD)
     GPIO.setwarnings(False)
-    motor_steps_dictionary = {}
-
-    # set GPIO pins
-    motor_one_direction_pin = 10
-    motor_one_step_pin = 8
-    motor_one_off_switch_pin = 22
-
-    motor_two_direction_pin = 38
-    motor_two_step_pin = 40
-    motor_two_off_switch_pin = 22
 
     # Create motor objects
-    motor_one = Motor('motor 1', motor_one_direction_pin, motor_one_step_pin, motor_one_off_switch_pin, 1)
-    motor_two = Motor('motor 2', motor_two_direction_pin, motor_two_step_pin, motor_two_off_switch_pin, 1)
+    BaseManager.register('Motor', Motor)
+    manager = BaseManager()
+    manager.start()
+    motor_one = manager.Motor(motor_name='motor 1', direction_pin=10, step_pin=8,
+                              off_switch_pin=22, direction=CW, step_amount=200)
+    motor_two = manager.Motor(motor_name='motor 2', direction_pin=38, step_pin=40,
+                              off_switch_pin=22, direction=CW, step_amount=200)
 
-    instructions = [{'velocity_1': 0.01, 'direction_1': 1, 'velocity_2': 0.02, 'direction_2': 1, 'step_duration': 2},
-                    {'velocity_1': 0.02, 'direction_1': 0, 'velocity_2': 0, 'direction_2': 1, 'step_duration': 3},
-                    {'velocity_1': 0, 'direction_1': 0, 'velocity_2': 0.002, 'direction_2': 0, 'step_duration': 3},
-                    {'velocity_1': 0.001, 'direction_1': 1, 'velocity_2': 0.002, 'direction_2': 0, 'step_duration': 3},
-                    {'velocity_1': 0.01, 'direction_1': 0, 'velocity_2': 0.01, 'direction_2': 0, 'step_duration': 2}]
+    instructions = [{'velocity_1': 0.01, 'direction_1': CW,
+                     'velocity_2': 0.02, 'direction_2': CW, 'step_duration': 10}]
+        # ,
+        #             {'velocity_1': 0.02, 'direction_1': CCW,
+        #              'velocity_2': 0, 'direction_2': CW, 'step_duration': 10},
+        #             {'velocity_1': 0, 'direction_1': CCW,
+        #              'velocity_2': 0.002, 'direction_2': CCW, 'step_duration': 10},
+        #             {'velocity_1': 0.001, 'direction_1': CW,
+        #              'velocity_2': 0.002, 'direction_2': CCW, 'step_duration': 10},
+        #             {'velocity_1': 0.01, 'direction_1': CCW,
+        #              'velocity_2': 0.01, 'direction_2': CCW, 'step_duration': 10}]
 
     for step, instruction in enumerate(instructions):
         velocity_1 = instruction['velocity_1']
@@ -83,7 +115,7 @@ if __name__ == "__main__":
 
         print(f'Step #{step + 1}, for an interval of {instruction["step_duration"]} seconds: \n'
               f'motor #1 will move in speed {instruction["velocity_1"]} in direction {instruction["direction_1"]}\n'
-              f'motor #2 will move in speed {instruction["velocity_2"]} in direction {instruction["direction_2"]}\n\n')
+              f'motor #2 will move in speed {instruction["velocity_2"]} in direction {instruction["direction_2"]}')
 
         motor_one_process = multiprocessing.Process(target=motor_one.motor_loop, args=(velocity_1, direction_1, ))
         motor_two_process = multiprocessing.Process(target=motor_two.motor_loop, args=(velocity_2, direction_2, ))
@@ -92,6 +124,9 @@ if __name__ == "__main__":
         motor_two_process.start()
 
         time.sleep(instruction['step_duration'])
+
+        print(f'Current Motor 1 status - {motor_one.get_dynamic_values()}')
+        print(f'Current Motor 2 status - {motor_two.get_dynamic_values()}\n\n')
 
         motor_one_process.kill()
         motor_two_process.kill()
